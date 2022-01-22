0fe60a225600260106828b64459f5d6057f5ac9e
Eugen <jasonfilipss@yahoo.com>
2022-01-22 16:12:23 +0200

    Finished the second function which reads user input and computer random choice and declares the winner

--- javascript.js
+++ javascript.js
@@ -4,21 +4,54 @@ it should pick a random number from 1 to 3 ( 0 to 2)
 return rock, paper or scissor 
 
 create a function called playARound with parameters (playerInput, computerChoice)
-function should compare the two inputs and decide the winner
+Make both parameters lowercase (for easier comparison)
+if playerInput and computerChoice are the same then draw;
+if playerInput is paper and computer choice is rock, player wins, paper beats rock;
+
+
 return the outcome
 
 
 
 
 */
-let computerChoice;
 
 function computerPlay(computerChoice){
     let randomNumber = Math.floor(Math.random()*3)+1;
-    computerChoice = (randomNumber == 1) ? 'Rock' : (randomNumber == 2) ? 'Paper' : 'Scrissors';
+    computerChoice = (randomNumber == 1) ? 'Rock' : (randomNumber == 2) ? 'Paper' : 'Scissors';
     return computerChoice;
 }
 
 function playARound(playerInput = prompt('Pick one: Rock, Paper, Scissors'), computerChoice){
+    computerChoice = computerPlay();
+    playerInput = playerInput.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
 
+    if (playerInput === computerChoice ){
+        return 'Nobody won, it\'s a tie'
+    }
+    else if (playerInput == 'rock'){
+        if (computerChoice == 'paper'){
+            return 'Computer won ' + computerChoice + ' beats ' + playerInput;
+        }
+        else{
+            return 'Player won ' + playerInput + ' beats ' + computerChoice;
+        }
+    }
+    else if (playerInput == 'paper'){
+        if (computerChoice == 'scissors'){
+            return 'Computer won ' + computerChoice + ' beats ' + playerInput;
+        }
+        else{
+            return 'Player won ' + playerInput + ' beats ' + computerChoice;
+        }
+    }
+    else if (playerInput == 'scissors'){
+        if (computerChoice == 'rock'){
+            return 'Computer won ' + computerChoice + ' beats ' + playerInput;
+        }
+        else{
+            return 'Player won ' + playerInput + ' beats ' + computerChoice;
+        }
+    }
 }
